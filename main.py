43b5dfee67a42f7d249b19d2b8d134f202e944d8
BECATRUE <becatrueyjh@gmail.com>
2023-11-07 04:54:53 +0900

    Implement to create a new room
    See also: #6

--- main.py
+++ main.py
@@ -35,3 +35,20 @@ async def create_user(name: str) -> int:
     """
     user_list.append(name)
     return len(user_list) - 1
+
+
+@app.post("/room/")
+async def create_room(name: str, user_id: int) -> int:
+    """Creates a new room with the given name.
+    
+    Args:
+        See RoomInfo.
+
+    Returns:
+        The created room ID, or -1 if failed.
+    """
+    if user_id not in user_list:
+        logger.exception("An unregistered user (ID: %d) tries to create a room", user_id)
+        return -1
+    room_list.append(RoomInfo(name=name, user_id=user_id))
+    return len(room_list) - 1
