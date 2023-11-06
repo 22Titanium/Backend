86561bcb7fb35ab5bc84dde0a24669103339d797
BECATRUE <becatrueyjh@gmail.com>
2023-11-07 04:30:39 +0900

    Implement to create a new user
    See also: #4

--- main.py
+++ main.py
@@ -2,11 +2,20 @@
 
 from fastapi import FastAPI
 
+user_list: list[str] = []
+
 app = FastAPI()
 
 
-# TODO(BECATRUE): This will be removed after test.
-@app.get("/")
-async def root():
-    """Temporary function for test."""
-    return "Hello, world!"
+@app.get("/user/me/")
+async def create_user(name: str) -> int:
+    """Creates a new user with the given name.
+    
+    Args:
+        name: The user name.
+
+    Returns:
+        The created user ID.
+    """
+    user_list.append(name)
+    return len(user_list) - 1
