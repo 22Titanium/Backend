2ca41e1559fff4ba6b35cae96682509617a7eeda
BECATRUE <becatrueyjh@gmail.com>
2023-11-12 03:44:49 +0900

    Update room info
    See also: #8

--- main.py
+++ main.py
@@ -142,4 +142,6 @@ async def enter_room(room_id: int, user_id: int) -> bool:
     if room.status == RoomInfo.Status.RUNNING:
         logger.exception("The room (ID: %d) is already running.", room_id)
         return False
+    room.player_list.append(user_id)
+    notify_modified(room_list_modified)
     return True
